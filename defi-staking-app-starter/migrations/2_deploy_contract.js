14219c5412150a2f376e5ebec4756762c8f1cc3e
knightpac <79067484+knightpac@users.noreply.github.com>
2021-10-19 14:14:01 +1300

    new update

--- defi-staking-app-starter/migrations/2_deploy_contract.js
+++ defi-staking-app-starter/migrations/2_deploy_contract.js
@@ -1,5 +1,15 @@
 const Tether = artifacts.require('Tether');
+const DecentralBank = artifacts.require('DecentralBank');
+const RWD = artifacts.require('RWD');
 
 module.exports = async function(deployer) {
+    // Deploy mock tether contract
     await deployer.deploy(Tether)
+
+    // Deploy Decentral Bank Contract
+    await deployer.deploy(DecentralBank)
+
+    // Deploy Reward Token Contract
+    await deployer.deploy(RWD)
 };
+
