3968475b942df3e48dc72dfbc9aa2ed509dacaaa
knightpac <79067484+knightpac@users.noreply.github.com>
2021-10-22 15:33:11 +1300

    Back End Completed

--- defi-staking-app-starter/src/contracts/DecentralBank.sol
+++ defi-staking-app-starter/src/contracts/DecentralBank.sol
@@ -9,8 +9,66 @@ contract DecentralBank {
     Tether public tether;
     RWD public rwd;
 
+    address[] public stakers;
+
+    mapping(address => uint256) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
+
     constructor(RWD _rwd, Tether _tether) public {
         rwd = _rwd;
         tether = _tether;
+        owner = msg.sender;
+    }
+
+    function depositTokens(uint256 _amount) public {
+        // Require the staking amount to be greater than 0
+        require(_amount > 0, "The amount can not be 0");
+
+        // Transfer tether tokens to this contract address for staking
+        tether.transferFrom(msg.sender, address(this), _amount);
+
+        // Update staking balance
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+        // check if they have staked
+        if (!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+
+        // Update Staking Balance
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
+    }
+
+    // Issue Rewards
+    function issueTokens() public {
+        // Require only the owner ot issue tokens
+        require(msg.sender == owner, "Caller must be contract owner");
+        for (uint256 i = 0; i < stakers.length; i++) {
+            address recipient = stakers[i];
+            // reward is the amount that the recipient is staking
+            uint256 balance = stakingBalance[recipient] / 9; // divide by 9 to create incentive for stakers
+            if (balance > 0) {
+                rwd.transfer(recipient, balance);
+            } // end of if loop
+        } //  end of for loop
+    } // end of issueTokens function
+
+    // Unstake Tokens
+    function unstakeTokens() public {
+        uint256 balance = stakingBalance[msg.sender];
+
+        // Require amount to be greater than zero
+        require(balance > 0, "Staking Balance Cant be Less Than Zero");
+
+        // Transfer tokens to the specified contract address from our bank
+        tether.transfer(msg.sender, balance);
+
+        // Reset Staking Balance
+        stakingBalance[msg.sender] = 0;
+
+        // Update Staking Status
+        isStaking[msg.sender] = false;
     }
 }
--- defi-staking-app-starter/test/decentralBank.tests.js
+++ defi-staking-app-starter/test/decentralBank.tests.js
@@ -1,5 +1,6 @@
 
 
+
 const RWD = artifacts.require('RWD')
 const Tether = artifacts.require('Tether')
 const DecentralBank = artifacts.require('DecentralBank')
@@ -8,13 +9,27 @@ require('chai')
 .use(require('chai-as-promised'))
 .should()
 
-contract('DecentralBank', (accounts) => {
-    let tether, rwd
+contract('DecentralBank', ([owner, customer]) => {
+    let tether, rwd, decentralBank
+
+    // function converts token numbers to wei
+    function tokens(number) {
+        return web3.utils.toWei(number, 'ether')
+    }
 
     // This code runs before any tests/descriptions.
     before( async () => {
+        
+        // Load Contracts
         tether = await Tether.new()
         rwd = await RWD.new()
+        decentralBank = await DecentralBank.new(rwd.address, tether.address)
+
+        // Transfer all tokens to decetral bank
+        await rwd.transfer(decentralBank.address, tokens('1000000'))
+
+        // Transfer 100 mock tether tokens to Investor accounts[1]
+        await tether.transfer(customer, tokens('100'), {from: owner})
     })
 
     describe('Mock Tether Deployment', async () => {
@@ -26,12 +41,88 @@ contract('DecentralBank', (accounts) => {
     })
     
     
-    describe('RWD Deployment', async () => {
+    describe('Reward Token Deployment', async () => {
         it('matches name successfully', async () => {
             
             const name = await rwd.name()
             assert.equal(name, 'Reward Token') 
         })
     }) 
+
+    // Test to see if tokens have been issued to decentral bank
+
+    describe('Decentral Bank Deployment', async () => {
+        it('matches name successfully', async () => {
+            
+            const name = await decentralBank.name()
+            assert.equal(name, 'Decentral Bank') 
+        })
+
+        // make sure there are tokens in bank
+        it('Contract has tokens issued', async () => {
+            let balance = await rwd.balanceOf(decentralBank.address)
+            assert.equal(balance, tokens('1000000'))
+
+        })
+
+    })
+    
+    describe('Yield Farming', async () => {
+        it('rewards tokens for staking', async () => {
+            let result
+            
+            // Check investor balance
+            result = await tether.balanceOf(customer)
+            assert.equal(result.toString(), tokens('100'), 'customer mock tether balance before staking')
+            
+            // Check Staking for Customer of 100 tokens
+            await tether.Approve(decentralBank.address, tokens('100'), {from: customer})
+            await decentralBank.depositTokens(tokens('100'), {from: customer})
+
+            // Check updated balance of Customer and make sure its 0 after staking 100 tokens--ERROR START
+            result = await tether.balanceOf(customer)
+            assert.equal(result.toString(), tokens('0'), 'Customer mock wallet balance after staking 100 tokens')
+
+            // Check updated balance of decentral bank
+            result = await tether.balanceOf(decentralBank.address)
+            assert.equal(result.toString(), tokens('100'), 'Decentral Bank wallet balance after staking from customer')
+
+            // Is staking update
+            result = await decentralBank.isStaking(customer)
+            assert.equal(result.toString(), 'true', 'Customer is staking status after staking to be true')
+
+            // Issue Tokens
+            await decentralBank.issueTokens({from: owner})
+
+            // Only Owner May Issue Tokens
+            await decentralBank.issueTokens({from: customer}).should.be.rejected;
+
+            // Unstake Tokens
+            await decentralBank.unstakeTokens({from: customer})
+
+            // ****Check unstaking Balances*****
+            
+                // Check updated balance of Customer and make sure its 0 after staking 100 tokens--ERROR START
+                result = await tether.balanceOf(customer)
+                assert.equal(result.toString(), tokens('100'), 'Customer mock wallet balance after unstaking')
+
+                // Check updated balance of decentral bank
+                result = await tether.balanceOf(decentralBank.address)
+                assert.equal(result.toString(), tokens('0'), 'Decentral Bank wallet balance after unstaking from customer')
+
+                // Is staking balance
+                result = await decentralBank.isStaking(customer)
+                assert.equal(result.toString(), 'false', 'Customer is staking status after unstaking to be false')
+
+
+        })
+
+
+
+    })  // End of Yield Farming Block  
+
+
+
+    
 
 }) 
